9b8b4ec903c907173ecfb9323d7e9083eefd4e1e
Arek Juszczyk <arek.juszczyk@ed.ac.uk>
2026-07-08 14:44:32 +0200

    Update class-ed-example-database.php

--- classes/class-ed-example-database.php
+++ classes/class-ed-example-database.php
@@ -28,7 +28,7 @@ class Ed_Example_Database implements Lti1p3\Interfaces\IDatabase
         $this->client_id = $client_id;
     }
 
-    public function findRegistrationByIssuer($iss): Lti1p3\LtiRegistration
+    public function findRegistrationByIssuer($iss): ?Lti1p3\ILtiRegistration
     {
         $platform = $this->get_platform($iss);
 
@@ -56,7 +56,7 @@ class Ed_Example_Database implements Lti1p3\Interfaces\IDatabase
         return $this->wpdb->get_row($this->wpdb->prepare($query, [ $iss, $this->client_id ]));
     }
 
-    public function findDeployment($iss, $deployment_id): Lti1p3\LtiDeployment
+    public function findDeployment($iss, $deployment_id): ?Lti1p3\LtiDeployment
     {
         $platform = $this->get_platform($iss);
 
