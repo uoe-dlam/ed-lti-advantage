731e77ccf64bdf512ec8f45b4c6f36b6a0260ade
Arek Juszczyk <arek.juszczyk@ed.ac.uk>
2026-07-08 15:26:35 +0200

    Update class-ed-example-database.php

--- classes/class-ed-example-database.php
+++ classes/class-ed-example-database.php
@@ -30,20 +30,22 @@ class Ed_Example_Database implements Lti1p3\Interfaces\IDatabase
         $this->client_id = $client_id;
     }
 
-    public function findRegistrationByIssuer($iss): ?ILtiRegistration
-    {
+    public function findRegistrationByIssuer(
+        string $iss,
+        ?string $clientId = null
+    ): ?ILtiRegistration {
         $platform = $this->get_platform($iss);
 
         if (empty($platform)) {
             wp_die('The platform that you linked from does not seem to be on out list of approved platforms. Please contact the site administrator.');
         }
 
-        return ILtiRegistration::new()
+        return Lti1p3\LtiRegistration::new()
                                    ->setAuthLoginUrl($platform->auth_login_url)
                                    ->setAuthTokenUrl($platform->auth_token_url)
                                    ->setClientId($this->client_id)
                                    ->setKeySetUrl($platform->key_set_url)
-                                   ->getIssuer($iss);
+                                   ->setIssuer($iss);
     }
 
     /**
@@ -58,7 +60,7 @@ class Ed_Example_Database implements Lti1p3\Interfaces\IDatabase
         return $this->wpdb->get_row($this->wpdb->prepare($query, [ $iss, $this->client_id ]));
     }
 
-    public function findDeployment($iss, $deployment_id): ?ILtiDeployment
+    public function findDeployment(string $iss, string $deploymentId, ?string $clientId = null): ?ILtiDeployment
     {
         $platform = $this->get_platform($iss);
 
